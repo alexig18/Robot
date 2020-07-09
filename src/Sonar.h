28153ed3a1869b0ea03716edb02e7d98f972bc3a
alexi <a.garbuz@alumni.ubc.ca>
2020-07-09 10:57:42 -0700

    sonar.h

--- src/Sonar.h
+++ src/Sonar.h
@@ -2,16 +2,19 @@
 #define Sonar_h
 
 #include "Arduino.h"
+#include "NewPing.h"
 
 class Sonar
-
+{
     public:
-    Sonar(int pinIn);
-    void read();
+    Sonar(int trigger, int echo, int maxDistance, int offset);
     int dist();
     private:
     // the underscore helps show the variable is private
-    int _pin;
+    int _trigger;
+    int _echo;
+    int _maxDistance;
+    int _offset;
 };
 
 #endif
