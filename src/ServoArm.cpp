980da81ef9e1524728acec23612d2dad3dd5f672
thoma <thomas.bichel@live.com>
2020-07-16 23:30:48 -0700

    Arm code modularized

--- src/ServoArm.cpp
+++ src/ServoArm.cpp
@@ -1,5 +1,10 @@
 #include <ServoArm.h>
 
+#define closedR 0
+#define closedL 0
+#define Lopen 0
+#define Ropen 0
+
 //default constructor
 ServoArm::ServoArm()
 {}
@@ -7,40 +12,44 @@ ServoArm::ServoArm()
 //perform setup
 void ServoArm::begin(int pinRight, int pinLeft)
 {
-    _servoR.write(0);
-    _servoL.write(0);
     _servoR.attach(pinRight);
     _servoL.attach(pinLeft);
+    _servoR.write(closedR);
+    _servoL.write(closedL);
+    delay(500);
+    _servoL.write(Lopen);
+    _servoR.write(Ropen);
+
 }
 
 void ServoArm::closeR()
 {
-    _servoR.write(_armAngle);
+    _servoR.write(closedR);
 }
 
 void ServoArm::closeL()
 {
-    _servoL.write(-_armAngle);
+    _servoL.write(closedL);
 }
 
 void ServoArm::openR()
 {
-    _servoR.write(-_armAngle);
+    _servoR.write(Ropen);
 }
 
 void ServoArm::openL()
 {
-    _servoL.write(_armAngle);
+    _servoL.write(Lopen);
 }
 
 void ServoArm::openBoth() {
-    openR();
-    openL();
+    this->ServoArm::openR();
+    this->ServoArm::openL();
 }
 
 void ServoArm::closeBoth() {
-    closeR();
-    closeL();
+    this->ServoArm::closeR();
+    this->ServoArm::closeL();
 }
 
 
