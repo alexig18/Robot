9270f9b45b91c1c7df817bad5453e7dfb8ca1a68
alexi <a.garbuz@alumni.ubc.ca>
2020-07-28 10:48:33 -0700

    can navigation but is no longer relevant

--- src/CanNavigation.cpp
+++ src/CanNavigation.cpp
@@ -1,37 +1,70 @@
 #include "Arduino.h"
 #include "CanNavigation.h"
 #include "ServoArm.h"
+#include "MotorControl.h"
+#include <string>
 
-CanNavigation::CanNavigation()
+#define DETECTION_DISTANCE 60 //in cm
+#define ITERATIONS 5
+#define TURNTIME 700 //ms
+
+CanNavigation:: CanNavigation(int echo,int trig,int max,MotorControl motors)
 {
-    _item = findItem();
-    if(_item) {
-        _isCan = isCan();
-        if(_isCan) {
-            collectCan();
+    
+    _sonar = new NewPing(trig,echo,max);
+    _motors = motors;
+}
+
+int CanNavigation::Measure()
+{
+    int dist = _sonar->convert_cm(_sonar->ping_median(ITERATIONS));
+    return dist;
+}
+
+
+bool CanNavigation::seek()
+{
+    _motors.move(400,-400);
+    if (CanNavigation::pingDelay(TURNTIME, DETECTION_DISTANCE)) {
+        return true;
+    }
+    _motors.move(-400,400);
+    if (CanNavigation::pingDelay(2 * TURNTIME, DETECTION_DISTANCE)) {
+        return true;
+    }
+    return false;
+}
+void  CanNavigation::move(String direction,int tim)
+{
+
+    if (direction.compareTo("left")){
+        _motors.move(-400,400);
+
+    }
+    else if (direction.compareTo("right")){
+        _motors.move(400,-400);
+    }
+
+
+}
+bool  CanNavigation::pingDelay(int interval, int threshold){
+    unsigned long  int currentTime = millis() , lastTime;
+    while(currentTime - lastTime < interval){
+        lastTime = millis();
+        int dist = CanNavigation::Measure();
+        if(dist <= threshold){
+            _motors.move(0,0);
+            //set global variable
+            _distance = dist;
+            return true;
         }
     }
+    return false;
 }
-
-bool CanNavigation::findItem()
+bool CanNavigation::findCan()
 {
-    // use sonar to find item within 8''
-}
 
-bool CanNavigation::isCan()
-{
-    /*
-    dist = sonar inpu
-
-    */
-}
-
-void CanNavigation::goToCan()
-{
-    // use whatever method we want to get to the can
-}
-
-void CanNavigation::collectCan() 
-{
-    // servo control to pick up can
+    //  Loops until can is found and then return true
+    delete _sonar;
+    return false;
 }
--- src/CanNavigation.h
+++ src/CanNavigation.h
@@ -1,26 +1,25 @@
 #ifndef CanNavigation_h
-#define CanNavigation_h
-
-#include "Arduino.h"
-#include "ServoArm.h"
-
-class CanNavigation
-{
-    public:
-    CanNavigation();
-    bool findItem(); 
-    // to locate something within sonar field of robot
-    bool isCan(); 
-    // to confirm whether something is a can or not
-    void goToCan(); 
-    // navigates to can
-    void collectCan(); 
-    // control can pick up
-    private:
-    // the underscore helps show the variable is private
-    int _sPin;
-    bool _item; // true if sonar detects an item within its range
-    bool _isCan; // true if isCan confirms the item found is a can
-};
-
-#endif
+    #define CanNavigation_h
+    #include "Arduino.h"
+    #include "ServoArm.h"
+    #include "NewPing.h" 
+    #include "MotorControl.h"
+    class CanNavigation
+    {
+        public:
+        CanNavigation(int echo,int trig,int max,MotorControl motors);
+        bool findCan();
+        private:
+        // the underscore helps show the variable is private
+        NewPing* _sonar;
+        MotorControl _motors;
+        int _distance;
+        int Measure(); 
+        // to locate something within sonar field of robot
+        bool seek();
+        // to confirm whether something is a can or not
+        void move(String direction,int tim);
+        // navigates to can
+        bool pingDelay (int interval, int threshold);
+    };
+    #endif
